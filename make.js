bab94ea2fa349a0efef07517d6ae01fbf111cd75
MaksimAliakhnovich <maksim.aliakhnovich@gmail.com>
2018-10-28 18:04:52 +0300

    feat: implement recursion function, add eslint and package

--- make.js
+++ make.js
@@ -1,13 +1,12 @@
 const make = function makeSmthWithArgs(args) {
   const arrArgs = [];
   function logic(...a) {
-    if (typeof a[0] != 'function') {
+    if (typeof a[0] !== 'function') {
       arrArgs.push(...a);
       return logic;
-    } else {
-      return arrArgs.reduce((acc, item) => a[0](acc, item));
     }
+    return arrArgs.reduce((acc, item) => a[0](acc, item));
   }
   logic(args);
   return logic;
-}
+};
